c152907caa4c458146749085e0a9ffb27dbf3733
pi6220na <myrlin2002@hotmail.com>
2017-10-10 15:55:19 -0500

    Initial commit

---
+++ model/currencyDB.js
@@ -0,0 +1,4 @@
+module.export = {
+    EUR : 0.85,
+    JPY : 112.63
+};
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var exchangerates = require('../model/currencyDB.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+    res.render('about', { name: 'My awesome site' });
+});
+
+
+router.get('/convert', function(req, res, next) {
+    var query = req.query;
+
+    console.log("exchange file " + exchangerates);
+
+    var dollars = req.query.dollars;
+    var toCurrency = req.query.to_currency;
+
+    var converted = dollars * exchangerates[toCurrency];
+
+
+
+    res.send(dollars + ' in ' + toCurrency + ' is ' + converted);
+});
+
+
+module.exports = router;
