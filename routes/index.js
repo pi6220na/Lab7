56963742c0cacbc56d463d7466fad675de8d0ad0
pi6220na <pi6220na@gmail.com>
2017-10-12 00:34:32 -0500

    refined convert page output

--- routes/index.js
+++ routes/index.js
@@ -19,33 +19,53 @@ router.get('/convert', function(req, res, next) {
     var toCurrency = req.query.to_currency;
     var fromCurrency = req.query.from_currency;
     var cRate;
+    var toLiteral;
+    var fromLiteral;
 
     if (fromCurrency === "EUR" && toCurrency === "USD") {
         cRate = "EtD";
-    }
+        fromLiteral = "Euros";
+        toLiteral = "Dollars";
+    } else
     if (fromCurrency === "EUR" && toCurrency === "JPY") {
         cRate = "EtY";
-    }
+        fromLiteral = "Euros";
+        toLiteral = "Yen";
+    } else
     if (fromCurrency === "EUR" && toCurrency === "EUR") {
         cRate = "EtE";
-    }
+        fromLiteral = "Euros";
+        toLiteral = "Euros";
+    } else
     if (fromCurrency === "USD" && toCurrency === "USD") {
         cRate = "DtD";
-    }
+        fromLiteral = "Dollars";
+        toLiteral = "Dollars";
+    } else
     if (fromCurrency === "USD" && toCurrency === "JPY") {
         cRate = "DtY";
-    }
+        fromLiteral = "Dollars";
+        toLiteral = "Yen";
+    } else
     if (fromCurrency === "USD" && toCurrency === "EUR") {
         cRate = "DtE";
-    }
+        fromLiteral = "Dollars";
+        toLiteral = "Euro";
+    } else
     if (fromCurrency === "JPY" && toCurrency === "USD") {
         cRate = "YtD";
-    }
+        fromLiteral = "Yen";
+        toLiteral = "Dollars";
+    } else
     if (fromCurrency === "JPY" && toCurrency === "JPY") {
         cRate = "YtY";
-    }
+        fromLiteral = "Yen";
+        toLiteral = "Yen";
+    } else
     if (fromCurrency === "JPY" && toCurrency === "EUR") {
         cRate = "YtE";
+        fromLiteral = "Yen";
+        toLiteral = "Euro";
     }
 
 
@@ -60,7 +80,9 @@ router.get('/convert', function(req, res, next) {
     res.render('results', {
         currency: currency,
         toCurrency: toCurrency,
+        toLiteral: toLiteral,
         fromCurrency: fromCurrency,
+        fromLiteral: fromLiteral,
         converted: converted}
     );
 });
